8a3d17c74707c96f459018ed17892d93fbf896a0
muslim2210 <bukhori.muslim2210@gmail.com>
2023-08-12 17:26:18 -0700

    menambahkan fitur search dan products section

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-// toggle class aktif
+// toggle class aktif untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika hamburger menu diklik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-// // klik diluar navbar untuk hilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// toogle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// // klik diluar elemen
+const hmb = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hmb.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
